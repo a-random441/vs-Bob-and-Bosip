17ec52b79f1fda6783ea19fb63c23e0562ef79d2
a-random441 <somerandomgithubacc@gmail.com>
2024-12-22 18:23:15 +0000

    how to fix crash (SO TRUE)

--- source/BootupState.hx
+++ source/BootupState.hx
@@ -130,7 +130,7 @@ class BootupState extends MusicBeatState
 
 		new FlxTimer().start(1, function(tmr:FlxTimer)
 		{
-			LoadingState.loadAndSwitchState(new VideoState2("assets/videos/desktop.webm", function() {
+			LoadingState.loadAndSwitchState(new VideoState2(SUtil.getStorageDirectory() + "assets/videos/desktop.webm", function() {
 				FlxG.switchState(new DesktopState());
 			}, 2000));
 		});
