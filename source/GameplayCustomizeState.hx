64b2f88511ceedfaf91edd9b730b7bf745fd5573
a-random441 <somerandomgithubacc@gmail.com>
2024-12-22 17:17:48 +0000

    ok

--- source/GameplayCustomizeState.hx
+++ source/GameplayCustomizeState.hx
@@ -40,7 +40,7 @@ class GameplayCustomizeState extends MusicBeatState
     private var camHUD:FlxCamera;
     
     public override function create() {
-        #if cpp
+        #if windows
 		// Updating Discord Rich Presence
 		DiscordClient.changePresence("Customizing Gameplay", null);
 		#end
