0acab716795e40c2c6b75aaf69a19331d417cd6f
a-random441 <somerandomgithubacc@gmail.com>
2024-12-22 16:58:28 +0000

    uhh

--- source/WebmHandler.hx
+++ source/WebmHandler.hx
@@ -2,13 +2,13 @@ package;
 
 import flixel.FlxG;
 import openfl.display.Sprite;
-#if extension-webm
+#if cpp
 import webm.*;
 #end
 
 class WebmHandler
 {
-	#if extension-webm
+	#if cpp
 	public var webm:WebmPlayer;
 	public var vidPath:String = "";
 	public var io:WebmIo;
