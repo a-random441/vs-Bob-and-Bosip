3666c1fca88d0fa502cbee8d21c480ba5eeaf27e
a-random441 <somerandomgithubacc@gmail.com>
2024-12-22 16:09:55 +0000

    bruh

--- source/DialogueBox.hx
+++ source/DialogueBox.hx
@@ -166,7 +166,15 @@ class DialogueBox extends FlxSpriteGroup
 			dialogueStarted = true;
 		}
 
-		if (FlxG.keys.justPressed.ANY  && dialogueStarted == true && canContinue)
+		#if mobileC
+                var justTouched:Bool = false;
+
+                for (touch in FlxG.touches.list)
+	        if (touch.justPressed)
+		justTouched = true;
+		#end
+
+		if (FlxG.keys.justPressed.ANY  #if mobileC || justTouched #end && dialogueStarted == true && canContinue)
 		{
 			remove(dialogue);
 				
